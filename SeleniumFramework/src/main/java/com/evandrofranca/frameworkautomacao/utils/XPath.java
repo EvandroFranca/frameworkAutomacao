97e8149f42baf6ecf5dfa0a285b7c713016b7c49
Evandro <evandro.franca00@gmail.com>
2020-07-04 21:50:14 -0300

    Adicionadas melhorias na classe TelaWeb e Xpath para montar xpaths

--- SeleniumFramework/src/main/java/com/evandrofranca/frameworkautomacao/utils/XPath.java
+++ SeleniumFramework/src/main/java/com/evandrofranca/frameworkautomacao/utils/XPath.java
@@ -1,13 +1,26 @@
 package com.evandrofranca.frameworkautomacao.utils;
 
 public class XPath {
-	
-	public static String montar(String tipo, String atributo, String valor) {
-		return "//"+tipo+"[@"+atributo+"='"+valor+"']";
-	}
-	
+
 	public static String montar(String tipo, String atributo, String valor, int index) {
-		return "(//"+tipo+"[@"+atributo+"='"+valor+"'])["+index+"]";
+		String xpath = "";
+		switch (atributo.toLowerCase()) {
+		case "text":
+			xpath = "//" + tipo + "[" + atributo + "()='" + valor + "']";
+			break;
+		case "textcontents":
+			xpath = "//" + tipo + "[contains(text(), '" + valor + "')]";
+			break;
+		default:
+			xpath = "//" + tipo + "[@" + atributo + "='" + valor + "']";
+			break;
+		}
+
+		if (index == 0) {
+			return xpath;
+		} else {
+			return "(" + xpath + ")[" + index + "]";
+		}
 	}
 
 }
